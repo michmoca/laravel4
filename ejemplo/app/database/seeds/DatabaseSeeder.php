ddac8b0c7fd72d3ecedfa701de3b8373a12e7377
michmoca <mich.montes@hotmail.com>
2013-06-06 07:19:43 -0600

    cambios que hice con tutos jaja
    pero aun estoy aprendiendo sarawaka

--- ejemplo/app/database/seeds/DatabaseSeeder.php
+++ ejemplo/app/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,14 @@
 <?php
-
+ 
 class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
-
-		// $this->call('UserTableSeeder');
-	}
-
+ 
+    public function run()
+    {
+        $this->call('SentrySeeder');
+        $this->command->info('Sentry tables seeded!');
+ 
+        $this->call('ContentSeeder');
+        $this->command->info('Content tables seeded!');
+    }
+ 
 }
